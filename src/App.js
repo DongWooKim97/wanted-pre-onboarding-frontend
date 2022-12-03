90574d39862bf6b170097f5238cdf685b9bd6341
DongWooKim97 <nanaukim@naver.com>
2022-12-03 22:17:32 +0900

    feat: 투두페이지 생성에 따른 새로운 라우터 및 주소 연결

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import SignupPage from './pages/SignupPage';
+import TodoPage from './pages/TodoPage';
 
 function App() {
 	return (
 		<Routes>
 			<Route path="/" element={<HomePage />}></Route>
 			<Route path="/signup" element={<SignupPage />}></Route>
+			<Route path="/todo" element={<TodoPage />}></Route>
 		</Routes>
 	);
 }
